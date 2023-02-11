a5788f0969904f32dc5736295ca4911855e5092d
EducationRemnant <69372912+EducationRemnant@users.noreply.github.com>
2023-02-11 06:30:55 -0600

    Header and footer edits

--- js/template.js
+++ js/template.js
@@ -6,13 +6,10 @@ class Header extends HTMLElement {
             <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-black">
                 <div class="container-fluid">
                     <a class="navbar-brand mb-0 header-long" href="https://www.unomaha.edu/index.php">
-                        <img class="d-inline-block align-top" src="images/logo.png" width="30" height="30" alt="UNO logo">
+                        <img class="d-inline-block align-top img-fluid" src="images/logo.png" width="30" height="30" alt="UNO logo">
                         | <img class="d-inline-block align-top" src="images/logo-big.PNG" height="30" alt="UNO logo">
                     </a>
-                    <a class="navbar-brand mb-0 header-short" href="https://www.unomaha.edu/index.php">
-                        <img class="d-inline-block align-top" src="images/logo.png" width="30" height="30" alt="UNO logo">
-                        | <img class="d-inline-block align-top" src="images/logo-big.PNG" width="220" height="30" alt="UNO logo">
-                    </a>
+
 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-menu">
                         <span class="navbar-toggler-icon"></span>
@@ -43,8 +40,8 @@ class Footer extends HTMLElement {
             <div class="container text-center">
                 <div>
                     <p>
-                        sida@unomaha.edu |
-                        106 Eppley Administration Building |
+                        sida@unomaha.edu <br>
+                        106 Eppley Administration Building <br>
                         6001 University Drive,
                         Omaha, Nebraska 68132
                     </p>
